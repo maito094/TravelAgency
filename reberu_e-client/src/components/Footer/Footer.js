9152e85ca9a38bbc19a58c90f91176cf893d9f5e
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-21 01:41:25 +0200

    Construct Footer React Component. Start building website with components

---
+++ reberu_e-client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="tm-bg-dark-blue">
+      <div className="container">
+        <div className="row">
+          <p className="col-sm-12 text-center tm-font-light tm-color-white p-4 tm-margin-b-0">
+            Copyright &copy; <span className="tm-current-year">2023</span> Travel Agency - ReberuE
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
