0ce11d25ba32660f51878718abca3b449a9b2fb4
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-24 10:45:18 +0200

    Create React Component Portfolio

---
+++ reberu_e-client/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,140 @@
+export const  Portfolio = () =>{
+
+    return(
+        <div className="tm-section tm-section-pad tm-bg-gray" id="tm-section-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8">
+              <div className="tm-article-carousel">
+                <article className="tm-bg-white mr-2 tm-carousel-item">
+                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Nunc in felis aliquet metus luctus iaculis</h3>
+                    </header>
+                    <p>
+                      Aliquam ac lacus volutpat, dictum risus at, scelerisque nulla. Nullam sollicitudin at augue venenatis eleifend. Nulla
+                      ligula ligula, egestas sit amet viverra id, iaculis sit amet ligula.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      Get More Info.
+                    </a>
+                  </div>
+                </article>
+                <article className="tm-bg-white mr-2 tm-carousel-item">
+                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Sed cursus dictum nunc quis molestie</h3>
+                    </header>
+                    <p>
+                      Pellentesque quis dui sit amet purus scelerisque eleifend sed ut eros. Morbi viverra blandit massa in varius. Sed nec
+                      ex eu ex tincidunt iaculis. Curabitur eget turpis gravida.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      View Detail
+                    </a>
+                  </div>
+                </article>
+                <article className="tm-bg-white mr-2 tm-carousel-item">
+                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Eget diam pellentesque interdum ut porta</h3>
+                    </header>
+                    <p>
+                      Aenean finibus tempor nulla, et maximus nibh dapibus ac. Duis consequat sed sapien venenatis consequat. Aliquam ac
+                      lacus volutpat, dictum risus at, scelerisque nulla.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      More Info.
+                    </a>
+                  </div>
+                </article>
+                <article className="tm-bg-white mr-2 tm-carousel-item">
+                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Lorem ipsum dolor sit amet, consectetur</h3>
+                    </header>
+                    <p>
+                      Suspendisse molestie sed dui eget faucibus. Duis accumsan sagittis tortor in ultrices. Praesent tortor ante, fringilla
+                      ac nibh porttitor, fermentum commodo nulla.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      Detail Info.
+                    </a>
+                  </div>
+                </article>
+                <article className="tm-bg-white mr-2 tm-carousel-item">
+                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Orci varius natoque penatibus et</h3>
+                    </header>
+                    <p>
+                      Pellentesque quis dui sit amet purus scelerisque eleifend sed ut eros. Morbi viverra blandit massa in varius. Sed nec
+                      ex eu ex tincidunt iaculis. Curabitur eget turpis gravida.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      Read More
+                    </a>
+                  </div>
+                </article>
+                <article className="tm-bg-white tm-carousel-item">
+                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
+                  <div className="tm-article-pad">
+                    <header>
+                      <h3 className="text-uppercase tm-article-title-2">Nullam sollicitudin at augue venenatis eleifend</h3>
+                    </header>
+                    <p>
+                      Aenean finibus tempor nulla, et maximus nibh dapibus ac. Duis consequat sed sapien venenatis consequat. Aliquam ac
+                      lacus volutpat, dictum risus at, scelerisque nulla.
+                    </p>
+                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
+                      More Details
+                    </a>
+                  </div>
+                </article>
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 tm-recommended-container">
+              <div className="tm-bg-white">
+                <div className="tm-bg-primary tm-sidebar-pad">
+                  <h3 className="tm-color-white tm-sidebar-title">Recommended Places</h3>
+                  <p className="tm-color-white tm-margin-b-0 tm-font-light">Enamel pin cliche tilde, kitsch and VHS thundercats</p>
+                </div>
+                <div className="tm-sidebar-pad-2">
+                  <a href="#" className="media tm-media tm-recommended-item">
+                    <img src="img/tn-img-01.jpg" alt="Image" />
+                    <div className="media-body tm-media-body tm-bg-gray">
+                      <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Europe</h4>
+                    </div>
+                  </a>
+                  <a href="#" className="media tm-media tm-recommended-item">
+                    <img src="img/tn-img-02.jpg" alt="Image" />
+                    <div className="media-body tm-media-body tm-bg-gray">
+                      <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Asia</h4>
+                    </div>
+                  </a>
+                  <a href="#" className="media tm-media tm-recommended-item">
+                    <img src="img/tn-img-03.jpg" alt="Image" />
+                    <div className="media-body tm-media-body tm-bg-gray">
+                      <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Africa</h4>
+                    </div>
+                  </a>
+                  <a href="#" className="media tm-media tm-recommended-item">
+                    <img src="img/tn-img-04.jpg" alt="Image" />
+                    <div className="media-body tm-media-body tm-bg-gray">
+                      <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">South America</h4>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
