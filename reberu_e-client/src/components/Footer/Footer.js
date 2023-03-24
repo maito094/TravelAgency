80b691721d2d6ea92187352952cdda855a94f584
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-24 19:14:31 +0200

    Add sticky scroll-top button and relative styling & eventHandler attached

--- reberu_e-client/src/components/Footer/Footer.js
+++ reberu_e-client/src/components/Footer/Footer.js
@@ -1,9 +1,25 @@
-import styles from '../Footer/Footer.module.css'
+import styles from '../Footer/Footer.module.css';
 
 export const Footer = () => {
+
+
+  const onClickScrollHome = (e) => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  };
+
+
   return (
-      
-    <footer className={`tm-bg-dark-blue ${styles.footer}`}>
+    <footer className={`tm-bg-dark-blue`}>
+      <div className={`${styles.stickyArrow}`}>
+        <a href="#" onClick={onClickScrollHome}>
+          <i className={`fa fa-chevron-circle-up `}></i>
+        </a>
+      </div>
       <div className="container">
         <div className="row">
           <p className="col-sm-12 text-center tm-font-light tm-color-white p-4 tm-margin-b-0">
@@ -12,6 +28,5 @@ export const Footer = () => {
         </div>
       </div>
     </footer>
-
   );
 };
