21a92af77e99e5c23970c23a682033754fa6cef0
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-24 19:36:48 +0200

    Restructuring - compile the necessary specific components under LandingPage component

--- reberu_e-client/src/App.js
+++ reberu_e-client/src/App.js
@@ -1,26 +1,15 @@
 import { Header } from './components/Header/Header';
+
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
-import { SearchBar } from './components/SearchBar/SearchBar';
-import { NewsletterSection } from './components/NewsletterSection/NewsletterSection';
-import { InfoSection } from './components/InformationSection/InformationSection';
-import { Portfolio } from './components/Portfolio/Portfolio';
-import { VideoSection } from './components/Common/VideoSection';
-import { BlogEntries } from './components/Blog/BlogEntries';
-import { Contacts } from './components/Contacts/Contacts';
+import { LandingPage } from './components/LandingPage/LandingPage';
 import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="tm-main-content" id="top">
       <Header />
-      <SearchBar />
-      <NewsletterSection />
-      <InfoSection />
-      <Portfolio />
-      <VideoSection />
-      <BlogEntries />
-      <Contacts />
+      <LandingPage />
       <Footer />
     </div>
   );
---
+++ reberu_e-client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,21 @@
+import { NewsletterSection } from './NewsletterSection/NewsletterSection';
+import { InfoSection } from './InformationSection/InformationSection';
+import { Portfolio } from './Portfolio/Portfolio';
+import { Contacts } from './Contacts/Contacts';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { VideoSection } from '../Common/VideoSection';
+import { BlogEntries } from '../Blog/BlogEntries'
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <SearchBar />
+      <NewsletterSection />
+      <InfoSection />
+      <Portfolio />
+      <VideoSection />
+      <BlogEntries />
+      <Contacts />
+    </div>
+  );
+};
