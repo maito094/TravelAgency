58bef29d74f070ef1c9fdaa04f5ba6aaedaa097e
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-25 23:55:08 +0200

    Install react-route-dom & prepare paths

--- reberu_e-client/src/App.js
+++ reberu_e-client/src/App.js
@@ -1,15 +1,23 @@
-import { Header } from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
 
+import { Header } from './components/Header/Header';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { LandingPage } from './components/LandingPage/LandingPage';
+import {CreateTopic} from './components/Blog/CreateTopic';
 import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="tm-main-content" id="top">
       <Header />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create-topic" element={<CreateTopic />} />
+        <Route path="/blogs" element={<CreateTopic />} />
+      </Routes>
       <Footer />
     </div>
   );
