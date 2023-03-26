ac02bdeca123d623789e2dab44c3d4f6cbcdbd81
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-27 01:54:39 +0300

    Update Login.js

--- reberu_e-client/src/components/Login/Login.js
+++ reberu_e-client/src/components/Login/Login.js
@@ -1,22 +1,58 @@
 import styles from './Login.module.css';
 
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/AuthContext';
+import { useForm } from '../../hooks/useForm';
+
+const LoginFormKeys = {
+  Email: 'email',
+  Password: 'password',
+};
+
 export const Login = () => {
+  const { onLoginSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      [LoginFormKeys.Email]: '',
+      [LoginFormKeys.Password]: '',
+    },
+    onLoginSubmit
+  );
+
   return (
     <div className={styles.divBody}>
       <div className={styles.loginBody}>
         <div className={styles.container}>
-          <form action="GET">
+          <form id="login" method="POST" onSubmit={onSubmit}>
             <div className={styles.title}>Login</div>
             <div className={styles['input-box']}>
-              <input type="text" placeholder="Enter Your Email" required />
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter Your Email"
+                name={LoginFormKeys.Email}
+                value={values[LoginFormKeys.Email]}
+                onChange={changeHandler}
+                required
+              />
               <div className={styles.underline}></div>
             </div>
             <div className={styles['input-box']}>
-              <input type="password" placeholder="Enter Your Password" required />
+              <input
+                type="password"
+                id="login-password"
+                name={LoginFormKeys.Password}
+                value={values[LoginFormKeys.Password]}
+                onChange={changeHandler}
+                placeholder="Enter Your Password"
+                required
+              />
               <div className={styles.underline}></div>
             </div>
             <div className={styles['input-box']}>
-              <input type="submit" name="" value="Continue" />
+              <input type="submit" value="Continue" />
             </div>
           </form>
           <div className={styles.option}>or Connect With Social Media</div>
