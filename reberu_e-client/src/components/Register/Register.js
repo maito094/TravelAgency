eaa483b6a30a65fe828e32c27381bde99c3252ee
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-27 01:54:46 +0300

    Update Register.js

--- reberu_e-client/src/components/Register/Register.js
+++ reberu_e-client/src/components/Register/Register.js
@@ -1,37 +1,70 @@
 import styles from './Register.module.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { useForm } from "../../hooks/useForm";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Register = () => {
+
+
+  const { onRegisterSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm({
+      email: '',
+      fullname:'',
+      mobile:'',
+      password: '',
+      confirmPassword: '',
+  }, onRegisterSubmit);
+
   return (
     <div className={styles.divBody}>
       <div className={styles.registerBody}>
-        <audio autoplay>
-          <source src="https://www.epidemicsound.com/track/TFQTXMbhOs/" type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
         <div className={styles['main-container']}>
           <div className={styles['main-forms']}>
             <div className={styles['signup-form']}>
-              <form className={styles['sign-back']} action="" method="">
+              <form className={styles['sign-back']} id="register" method="post" onSubmit={onSubmit}>
                 <h1>sign UP</h1>
                 <div className={styles['signup-row']}>
                   <i className="fa fa-user"></i>
                   <label htmlFor="fullname"></label>
-                  <input type="text" name="fullname" value="" placeholder="FULL NAME" />
+                  <input
+                   type="text"
+                   name="fullname"
+                   placeholder="FULL NAME"
+                   value={values.fullname}
+                  onChange={changeHandler} />
                 </div>
                 <div className={styles['signup-row']}>
                   <i className="fa fa-envelope-o"></i>
                   <label htmlFor="email"></label>
-                  <input type="text" name="email" value="" placeholder="EMAIL" />
+                  <input 
+                  type="text"
+                   name="email"
+                   placeholder="EMAIL"
+                   value={values.email}
+                   onChange={changeHandler}
+                   />
                 </div>
                 <div className={styles['signup-row']}>
                   <i className="fa fa-phone"></i>
                   <label htmlFor="mobile"></label>
-                  <input type="text" name="mobile" value="" placeholder="MOBILE" />
+                  <input 
+                  type="text" 
+                  name="mobile" 
+                  placeholder="MOBILE"
+                  value={values.mobile}
+                  onChange={changeHandler}  />
                 </div>
                 <div className={styles['signup-row']}>
                   <i className="fa fa-key"></i>
                   <label htmlFor="pass"></label>
-                  <input type="password" name="password" value="" placeholder="PASSWORD" />
+                  <input 
+                  type="password"
+                   name="password" 
+                   placeholder="PASSWORD"
+                   value={values.password}
+                   onChange={changeHandler} />
                 </div>
                 <div className={styles['input-box']}>
                 <label htmlFor="continue"></label>
@@ -44,7 +77,7 @@ export const Register = () => {
                     <span>Remember</span>
                   </div>
                   <div className={styles['remember']}>
-                    <a href="#">Already Have Account ?</a>
+                    <Link to="/login">Already Have Account ?</Link>
                   </div>
                 </div>
               </form>
