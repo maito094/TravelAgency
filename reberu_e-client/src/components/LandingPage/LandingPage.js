04e15911fdb8ebb9ae0157fab50b07cbf57d2a31
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-26 07:24:14 +0300

    + to last commit for PolygonSection

--- reberu_e-client/src/components/LandingPage/LandingPage.js
+++ reberu_e-client/src/components/LandingPage/LandingPage.js
@@ -1,16 +1,24 @@
-import { NewsletterSection } from './NewsletterSection/NewsletterSection';
 import { InfoSection } from './InformationSection/InformationSection';
 import { Portfolio } from './Portfolio/Portfolio';
 import { Contacts } from './Contacts/Contacts';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { VideoSection } from '../Common/VideoSection';
+import { PolygonSection } from '../Common/PolygonSection';
 import { BlogEntries } from '../Blog/BlogEntries';
+
+
+const Newletters ={
+  title:'We are here to help you?',
+  subtitle:'Subscribe to get our newsletters',
+  path:'/',
+  btnMsg:'Subscribe Newletters'
+}
 
 export const LandingPage = () => {
   return (
     <div>
       <SearchBar />
-      <NewsletterSection />
+      <PolygonSection {...Newletters} />
       <InfoSection />
       <Portfolio />
       <VideoSection />
