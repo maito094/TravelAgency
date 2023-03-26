eab6354359da2d76fe7558d1966ea62720c64995
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-26 07:21:40 +0300

    construct Article Common React component.
    Redo with test data Portfolio Component & Create Common Article component used in Porftolio and in Blog

---
+++ reberu_e-client/src/components/Common/Article.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styles from "./Article.module.css";
+
+export const Article =({
+  dest,
+  title,
+  img,
+  content,
+  _id,
+}
+)=>{
+
+    return(
+
+        <article className={`tm-bg-white m-2 ${styles['flex-articles']}`}>
+        <img src={img} alt="" className="img-fluid" />
+        <div className={`tm-article-pad ${styles['flex-articles']}`}>
+          <header>
+            <h3 className="text-uppercase tm-article-title-2">{title}</h3>
+          </header>
+          <p>
+            {content}
+          </p>
+          <Link to={`${dest}/${_id}`} className="text-uppercase btn-primary tm-btn-primary">
+            Get More Info.
+          </Link>
+        </div>
+      </article>
+    )
+}
--- reberu_e-client/src/components/LandingPage/Portfolio/Portfolio.js
+++ reberu_e-client/src/components/LandingPage/Portfolio/Portfolio.js
@@ -1,3 +1,14 @@
+import {Article} from '../../Common/Article'
+
+
+const propsTest = {
+  dest:"http://localhost:3030/data/topic",
+  title:"Nunc in felis aliquet metus luctus iaculis",
+  img:"img/img-01.jpg",
+  content:"Aliquam ac lacus volutpat, dictum risus at, scelerisque nulla. Nullam sollicitudin at augue venenatis eleifend. Nulla ligula ligula, egestas sit amet viverra id, iaculis sit amet ligula.",
+  _id:'1'
+}
+
 export const  Portfolio = () =>{
 
     return(
@@ -6,96 +17,13 @@ export const  Portfolio = () =>{
           <div className="row">
             <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8">
               <div className="tm-article-carousel">
-                <article className="tm-bg-white mr-2 tm-carousel-item">
-                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Nunc in felis aliquet metus luctus iaculis</h3>
-                    </header>
-                    <p>
-                      Aliquam ac lacus volutpat, dictum risus at, scelerisque nulla. Nullam sollicitudin at augue venenatis eleifend. Nulla
-                      ligula ligula, egestas sit amet viverra id, iaculis sit amet ligula.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      Get More Info.
-                    </a>
-                  </div>
-                </article>
-                <article className="tm-bg-white mr-2 tm-carousel-item">
-                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Sed cursus dictum nunc quis molestie</h3>
-                    </header>
-                    <p>
-                      Pellentesque quis dui sit amet purus scelerisque eleifend sed ut eros. Morbi viverra blandit massa in varius. Sed nec
-                      ex eu ex tincidunt iaculis. Curabitur eget turpis gravida.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      View Detail
-                    </a>
-                  </div>
-                </article>
-                <article className="tm-bg-white mr-2 tm-carousel-item">
-                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Eget diam pellentesque interdum ut porta</h3>
-                    </header>
-                    <p>
-                      Aenean finibus tempor nulla, et maximus nibh dapibus ac. Duis consequat sed sapien venenatis consequat. Aliquam ac
-                      lacus volutpat, dictum risus at, scelerisque nulla.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      More Info.
-                    </a>
-                  </div>
-                </article>
-                <article className="tm-bg-white mr-2 tm-carousel-item">
-                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Lorem ipsum dolor sit amet, consectetur</h3>
-                    </header>
-                    <p>
-                      Suspendisse molestie sed dui eget faucibus. Duis accumsan sagittis tortor in ultrices. Praesent tortor ante, fringilla
-                      ac nibh porttitor, fermentum commodo nulla.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      Detail Info.
-                    </a>
-                  </div>
-                </article>
-                <article className="tm-bg-white mr-2 tm-carousel-item">
-                  <img src="img/img-01.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Orci varius natoque penatibus et</h3>
-                    </header>
-                    <p>
-                      Pellentesque quis dui sit amet purus scelerisque eleifend sed ut eros. Morbi viverra blandit massa in varius. Sed nec
-                      ex eu ex tincidunt iaculis. Curabitur eget turpis gravida.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      Read More
-                    </a>
-                  </div>
-                </article>
-                <article className="tm-bg-white tm-carousel-item">
-                  <img src="img/img-02.jpg" alt="Image" className="img-fluid" />
-                  <div className="tm-article-pad">
-                    <header>
-                      <h3 className="text-uppercase tm-article-title-2">Nullam sollicitudin at augue venenatis eleifend</h3>
-                    </header>
-                    <p>
-                      Aenean finibus tempor nulla, et maximus nibh dapibus ac. Duis consequat sed sapien venenatis consequat. Aliquam ac
-                      lacus volutpat, dictum risus at, scelerisque nulla.
-                    </p>
-                    <a href="#" className="text-uppercase btn-primary tm-btn-primary">
-                      More Details
-                    </a>
-                  </div>
-                </article>
+                <Article {...propsTest}/>
+                <Article {...propsTest}/>
+                <Article {...propsTest}/>
+                <Article {...propsTest}/>
+                <Article {...propsTest}/>
+                <Article {...propsTest}/>
+             
               </div>
             </div>
 
@@ -107,25 +35,25 @@ export const  Portfolio = () =>{
                 </div>
                 <div className="tm-sidebar-pad-2">
                   <a href="#" className="media tm-media tm-recommended-item">
-                    <img src="img/tn-img-01.jpg" alt="Image" />
+                    <img src="img/tn-img-01.jpg" alt="" />
                     <div className="media-body tm-media-body tm-bg-gray">
                       <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Europe</h4>
                     </div>
                   </a>
                   <a href="#" className="media tm-media tm-recommended-item">
-                    <img src="img/tn-img-02.jpg" alt="Image" />
+                    <img src="img/tn-img-02.jpg" alt="" />
                     <div className="media-body tm-media-body tm-bg-gray">
                       <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Asia</h4>
                     </div>
                   </a>
                   <a href="#" className="media tm-media tm-recommended-item">
-                    <img src="img/tn-img-03.jpg" alt="Image" />
+                    <img src="img/tn-img-03.jpg" alt="" />
                     <div className="media-body tm-media-body tm-bg-gray">
                       <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">Africa</h4>
                     </div>
                   </a>
                   <a href="#" className="media tm-media tm-recommended-item">
-                    <img src="img/tn-img-04.jpg" alt="Image" />
+                    <img src="img/tn-img-04.jpg" alt="" />
                     <div className="media-body tm-media-body tm-bg-gray">
                       <h4 className="text-uppercase tm-font-semibold tm-sidebar-item-title">North America</h4>
                     </div>
