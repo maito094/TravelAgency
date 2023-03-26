f7302c53be16e5bcccfe95d1aca5c37d1707eeeb
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-26 10:49:43 +0300

    Fix Play/Pause of Video Section

--- reberu_e-client/src/components/Common/VideoSection.js
+++ reberu_e-client/src/components/Common/VideoSection.js
@@ -1,9 +1,25 @@
+const onPlay = (e) => {
+  let vid = document.querySelector('.tmVideo');
+
+  if (e.currentTarget.classList.contains('tm-btn-play')) {
+    
+    vid.play();
+    e.currentTarget.setAttribute("style","content-visibility:hidden;");
+    e.currentTarget.nextSibling.setAttribute("style","display: inline;");
+  }
+  else if(e.currentTarget.classList.contains('tm-btn-pause')){
+    vid.pause();
+    e.currentTarget.setAttribute("style","content-visibility:hidden;");
+    e.currentTarget.previousSibling.setAttribute("style","display: inline;");
+  }
+};
+
 export const VideoSection = () => {
   return (
     <div className="tm-bg-video">
       <div className="overlay">
-        <i className="fa fa-5x fa-play-circle tm-btn-play"></i>
-        <i className="fa fa-5x fa-pause-circle tm-btn-pause"></i>
+        <i onClick={(e) => {onPlay(e)}} className="fa fa-5x fa-play-circle tm-btn-play"></i>
+        <i onClick={(e) => {onPlay(e)}} className="fa fa-5x fa-pause-circle tm-btn-pause"></i>
       </div>
       <video controls loop className="tmVideo">
         <source src="videos/video.mp4" type="video/mp4" />
