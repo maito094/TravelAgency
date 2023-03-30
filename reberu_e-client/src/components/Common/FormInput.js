ca690fb0ec28070483b7b7e6564390c78d4c9cc6
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-31 02:48:01 +0300

    Fix abstraction. Add additional handler onBlurCapture. Nvertheless both files are not used.
    The Component and hook were exchange and replaced with simpler components, because of the complexity of the task at hand - to dynamically add additional form Inputs and manage the state, without difficult pass around of it between components and hooks.

--- reberu_e-client/src/components/Common/FormInput.js
+++ reberu_e-client/src/components/Common/FormInput.js
@@ -1,15 +1,23 @@
 import { useInput } from '../../hooks/useInput';
 
-export const FormInput = ({id, label, labelValue, type, name,  initValue }) => {
+export const FormInput = ({id,
+   label,
+   labelValue,
+   type,
+   name,
+   onChangeHandler,
+    initValue
+ }) => {
 
-  const { values, changeHandler } = useInput({
+
+  const { values, changeHandler, onAckInput } = useInput({
     [name]: initValue,
-  });
+  },onChangeHandler);
 
   return (
     <div className="form-group">
       <label htmlFor={label}>{labelValue}:</label>
-      <input type={type} id={name+id} name={name} className="form-control" placeholder="" required value={values[name]} onChange={changeHandler} />
+      <input type={type} id={name+id} name={name} className="form-control" placeholder="" required value={values[name]} onChange={changeHandler} onBlurCapture={onAckInput} />
     </div>
   );
 };
--- reberu_e-client/src/hooks/useInput.js
+++ reberu_e-client/src/hooks/useInput.js
@@ -1,14 +1,24 @@
 import { useState, useEffect } from 'react';
 
-export const useInput = (initialValues) => {
+export const useInput = (initialValues,onChangeHandler) => {
     const [values, setValues] = useState(initialValues);
 
     const changeHandler = (e) => {
         setValues(state => ({...state, [e.target.name]: e.target.value}));
+
     };
+
+    const onAckInput = (e) => {
+
+        e.preventDefault();
+
+        onChangeHandler(values);
+    }
+
 
     return {
         values,
         changeHandler,
+        onAckInput
     };
 };
