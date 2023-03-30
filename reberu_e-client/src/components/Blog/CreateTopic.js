66d6e9bd1ca8b10ce0a0a51d05b3140b7c17098e
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-30 21:44:39 +0300

    Working dynamic State Inputs, but not final.
    The dynamic input's values are not accessed to parent component which makes the service request for creation of topic.

--- reberu_e-client/src/components/Blog/CreateTopic.js
+++ reberu_e-client/src/components/Blog/CreateTopic.js
@@ -4,22 +4,52 @@ import { useService } from '../../hooks/useService';
 import { useForm } from '../../hooks/useForm';
 
 export const CreateTopic = ({ onCreateTopicSubmit }) => {
-  const [fieldsList, setFieldsList] = useState([
-    {
-      Do: {
-        name: 'Bright Angel Trail',
-        img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/84/4e/38/caption.jpg?w=1400&h=-1&s=1',
-      },
-    },
-  ]);
-
-  const { values, changeHandler, onSubmit } = useForm(
+  let [doObj, setDoObj] = useState([
     {
       name: '',
       img: '',
     },
-    onCreateTopicSubmit
-  );
+  ]);
+
+  let [stayObj, setStayObj] = useState([
+    {
+      name: '',
+      img: '',
+    },
+  ]);
+
+  let [eatObj, setEatObj] = useState([
+    {
+      name: '',
+      img: '',
+    },
+  ]);
+  const { values, changeHandler } = useForm({
+    title: '',
+    description: '',
+    img: '',
+    continent: '',
+  });
+
+
+  const Do = {};
+  const Stay = {};
+  const Eat = {};
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+
+
+    console.log(doObj);
+    console.log(stayObj);
+    console.log(eatObj);
+    // TODO - Add data from InputForm and useForm together
+    // and compose whole the data to send and create Topic
+    // Object.assign(doObj);
+    console.log(values);
+    //onCreateTopicSubmit(values);
+  };
 
   const onClick = (e) => {
     e.preventDefault();
@@ -31,17 +61,36 @@ export const CreateTopic = ({ onCreateTopicSubmit }) => {
 
     if (e) {
       let id = e.target.getAttribute('id');
-      if (id === 'todoBtn' && fieldsList.length < 4) {
-        let doObj = { do: '' };
-        setFieldsList((x) => [...x, doObj]);
-        console.log(fieldsList);
+      if (id === 'todoBtn' && doObj.length < 4) {
+        let doObjCurrent = {
+          name: '',
+          img: '',
+        };
+        setDoObj((x) => [...x, doObjCurrent]);
+        console.log(doObj);
+      }
+      if (id === 'tostayBtn' && stayObj.length < 4) {
+        let stayObjCurrent = {
+          name: '',
+          img: '',
+        };
+        setStayObj((x) => [...x, stayObjCurrent]);
+        console.log(stayObj);
+      }
+      if (id === 'toeatBtn' && eatObj.length < 4) {
+        let eatObjCurrent = {
+          name: '',
+          img: '',
+        };
+        setEatObj((x) => [...x, eatObjCurrent]);
+        console.log(eatObj);
       }
     }
   };
 
   return (
     <div
-      style={{ height: '90vh', marginTop: '1.8rem' }}
+      style={{ overflow: 'scroll', height: '90vh', marginTop: '1.8rem' }}
       className="tm-section tm-section-pad tm-bg-img tm-position-relative"
       id="tm-section-6"
     >
@@ -50,56 +99,142 @@ export const CreateTopic = ({ onCreateTopicSubmit }) => {
       </h1>
       <div className="container ie-h-align-center-fix">
         <div className="tm-bg-white tm-p-4">
-          <form action="index.html" method="post" className="contact-form">
+          <form onSubmit={onSubmit} method="post" className="contact-form">
             <div className="form-group">
               <label htmlFor="title">Title:</label>
-              <input type="text" id="title" name="title" className="form-control" placeholder="Title" required />
+              <input
+                value={values.title}
+                onChange={changeHandler}
+                type="text"
+                id="title"
+                name="title"
+                className="form-control"
+                placeholder="Title"
+                required
+              />
             </div>
             <div className="form-group">
               <label htmlFor="description">Description:</label>
-              <input type="email" id="description" name="description" className="form-control" placeholder="Content description" required />
+              <input
+                value={values.description}
+                onChange={changeHandler}
+                type="email"
+                id="description"
+                name="description"
+                className="form-control"
+                placeholder="Content description"
+                required
+              />
             </div>
             <div className="form-group">
               <label htmlFor="img">Image:</label>
-              <input type="text" id="img" name="img" className="form-control" placeholder="Image URL" required />
+              <input
+                value={values.img}
+                onChange={changeHandler}
+                type="text"
+                id="img"
+                name="img"
+                className="form-control"
+                placeholder="Image URL"
+                required
+              />
             </div>
             <div className="form-group">
               <label htmlFor="continent">Continent:</label>
-              <input type="text" id="continent" name="continent" className="form-control" placeholder="Continent" required />
+              <input
+                value={values.continent}
+                onChange={changeHandler}
+                type="text"
+                id="continent"
+                name="continent"
+                className="form-control"
+                placeholder="Continent"
+                required
+              />
             </div>
             <div onClick={onClick} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
               <button id="todoBtn">TO DO +</button>
 
-              {fieldsList.map(
+              {doObj.map(
                 (singleField, index) =>
                   index > 0 && (
                     <div className="test" key={index}>
                       <FormInput
+                        id={index}
                         label={'Name'}
                         labelValue={'Name'}
                         type={'text'}
                         name={'name'}
-                        onChange={onSubmit}
+                        // onChange={useInput} // TO DO USE HERE TO PASS SETDATA HANDLER, SO THERE IS ACCESS TO STATE COMPONENT INPUT DATA ! ! !
                         initValue={singleField.name}
                       />
                       <FormInput
+                        id={index}
                         label={'Image'}
                         labelValue={'Image'}
                         type={'text'}
                         name={'img'}
-                        onChange={onSubmit}
+                        // onChange={useInput}
                         initValue={singleField.img}
                       />
                     </div>
                   )
               )}
               <button id="tostayBtn">TO STAY +</button>
+              {stayObj.map(
+                (singleField, index) =>
+                  index > 0 && (
+                    <div className="test" key={index}>
+                      <FormInput
+                        id={index}
+                        label={'Name'}
+                        labelValue={'Name'}
+                        type={'text'}
+                        name={'name'}
+                        // onChange={useInput}
+                        initValue={singleField.name}
+                      />
+                      <FormInput
+                        id={index}
+                        label={'Image'}
+                        labelValue={'Image'}
+                        type={'text'}
+                        name={'img'}
+                        // onChange={useInput}
+                        initValue={singleField.img}
+                      />
+                    </div>
+                  )
+              )}
               <button id="toeatBtn">TO EAT +</button>
+              {eatObj.map(
+                (singleField, index) =>
+                  index > 0 && (
+                    <div className="test" key={index}>
+                      <FormInput
+                        id={index}
+                        label={'Name'}
+                        labelValue={'Name'}
+                        type={'text'}
+                        name={'name'}
+                        // onChange={useInput}
+                        initValue={singleField.name}
+                      />
+                      <FormInput
+                        id={index}
+                        label={'Image'}
+                        labelValue={'Image'}
+                        type={'text'}
+                        name={'img'}
+                        // onChange={useInput}
+                        initValue={singleField.img}
+                      />
+                    </div>
+                  )
+              )}
             </div>
 
-            <button type="submit" className="btn btn-primary tm-btn-primary">
-              Create Topic
-            </button>
+            <input value="Create Topic" type="submit" className="btn btn-primary tm-btn-primary" />
           </form>
         </div>
       </div>
