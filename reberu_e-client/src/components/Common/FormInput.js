1a4276600f1f7dcf860c95eaf5b5c9c8a22fcb1f
Metodi Bozhkov <mlbozhkov@gmail.com>
2023-03-30 15:13:06 +0300

    use new custom hook useInput to handle passed formInput component

--- reberu_e-client/src/components/Common/FormInput.js
+++ reberu_e-client/src/components/Common/FormInput.js
@@ -1,8 +1,15 @@
-export const FormInput = ({ label, labelValue, type, name, onChange, initValue }) => {
+import { useInput } from '../../hooks/useInput';
+
+export const FormInput = ({id, label, labelValue, type, name,  initValue }) => {
+
+  const { values, changeHandler } = useInput({
+    [name]: initValue,
+  });
+
   return (
     <div className="form-group">
       <label htmlFor={label}>{labelValue}:</label>
-      <input type={type} id={name} name={name} className="form-control" placeholder="" required value={initValue} onChange={onChange} />
+      <input type={type} id={name+id} name={name} className="form-control" placeholder="" required value={values[name]} onChange={changeHandler} />
     </div>
   );
 };
